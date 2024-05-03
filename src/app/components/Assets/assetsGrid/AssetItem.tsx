e76f18ccf36ddf0775f75b4cf3d8dd655423a48f
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-05-03 10:08:16 -0300

    feat: fix curate stack

--- src/app/components/Assets/assetsGrid/AssetItem.tsx
+++ src/app/components/Assets/assetsGrid/AssetItem.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { AWS_BASE_URL_S3 } from '@/constants/aws';
 import { Asset } from '@/features/assets/types';
 import { useState } from 'react';
+import { MediaRenderer } from '../components/MediaRenderer';
 
 interface Props {
     assetView: Asset;
@@ -15,8 +16,6 @@ interface Props {
 }
 
 const AssetItem = ({ assetView, asset, isCurated, checkedCurate, handleChangeCurate, handleClickImage }: Props) => {
-    const [file, setFile] = useState(`${AWS_BASE_URL_S3}/${asset?.formats?.preview?.path}`);
-
     return (
         <Box
             sx={{
@@ -26,36 +25,12 @@ const AssetItem = ({ assetView, asset, isCurated, checkedCurate, handleChangeCur
             }}
         >
             <BlankCard className="hoverCard">
-                <Typography>
-                    {asset?.formats?.preview?.path.includes('mp4') ? (
-                        <video
-                            width="250"
-                            height="250"
-                            autoPlay
-                            muted
-                            loop
-                            style={{
-                                objectFit: 'cover',
-                            }}
-                        >
-                            <source src={file} type="video/mp4" />
-                        </video>
-                    ) : (
-                        <Image
-                            onClick={handleClickImage}
-                            src={file}
-                            alt="img"
-                            style={{
-                                objectFit: 'cover',
-                            }}
-                            width={250}
-                            height={250}
-                            onError={(e) => {
-                                setFile('https://via.placeholder.com/250');
-                            }}
-                        />
-                    )}
-                </Typography>
+                <Box width={250} height={250}>
+                    <MediaRenderer
+                        src={`${AWS_BASE_URL_S3}/${asset?.formats?.preview?.path}`}
+                        fallbackSrc={'https://via.placeholder.com/250'}
+                    />
+                </Box>
 
                 <CardContent sx={{ p: 3, pt: 2 }}>
                     <Typography variant="h6" onClick={handleClickImage} sx={{ cursor: 'pointer' }}>
