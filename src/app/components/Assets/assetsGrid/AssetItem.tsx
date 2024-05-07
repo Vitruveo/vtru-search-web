ac3d644b8615259821f7e9210eac58c511d4a9bb
Matheus Paice <matheus.paice@jbtec.com.br>
2024-05-07 07:55:08 -0300

    fix: correct rule publish

--- src/app/components/Assets/assetsGrid/AssetItem.tsx
+++ src/app/components/Assets/assetsGrid/AssetItem.tsx
@@ -9,7 +9,7 @@ interface Props {
     asset: Asset;
     isCurated: boolean;
     checkedCurate: boolean;
-    handleChangeCurate(event: React.ChangeEvent<HTMLInputElement>): void;
+    handleChangeCurate(): void;
     handleClickImage(): void;
 }
 
@@ -21,9 +21,13 @@ const AssetItem = ({ assetView, asset, isCurated, checkedCurate, handleChangeCur
                 maxWidth: 250,
                 cursor: 'pointer',
             }}
+            onClick={() => {
+                if (isCurated) handleChangeCurate();
+                else handleClickImage();
+            }}
         >
             <BlankCard className="hoverCard">
-                <Box width={250} height={250} onClick={handleClickImage} borderRadius='8px 8px 0 0'>
+                <Box width={250} height={250} borderRadius="8px 8px 0 0">
                     <MediaRenderer
                         src={`${AWS_BASE_URL_S3}/${asset?.formats?.preview?.path}`}
                         fallbackSrc={'https://via.placeholder.com/250'}
@@ -31,16 +35,28 @@ const AssetItem = ({ assetView, asset, isCurated, checkedCurate, handleChangeCur
                 </Box>
 
                 <CardContent sx={{ p: 3, pt: 2 }}>
-                    <Typography variant="h6" onClick={handleClickImage} sx={{ cursor: 'pointer' }}>
-                        {asset?.assetMetadata?.context?.formData?.title || 'No Title'}
-                    </Typography>
                     <Stack direction="row" alignItems="center" justifyContent="space-between" mt={1}>
-                        <Typography variant="h6" height={25}>
+                        <Typography variant="h6" sx={{ cursor: 'pointer' }}>
+                            {asset?.assetMetadata?.context?.formData?.title || 'No Title'}
+                        </Typography>
+                        {isCurated && <Checkbox style={{ padding: 0 }} checked={checkedCurate} />}
+                    </Stack>
+                    <Stack direction="row" alignItems="center" justifyContent="space-between" mt={1}>
+                        {Array.isArray(asset?.assetMetadata?.creators?.formData) &&
+                            asset?.assetMetadata?.creators?.formData?.length > 0 && (
+                                <a
+                                    href="#"
+                                    onClick={(event) => {
+                                        event.stopPropagation();
+                                    }}
+                                    style={{ textDecoration: 'underline', padding: 5, paddingLeft: 0 }}
+                                >
+                                    {asset?.assetMetadata?.creators?.formData[0].name || 'No creator'}
+                                </a>
+                            )}
+                        <Typography variant="h6" minWidth={42} height={25}>
                             $ 150
                         </Typography>
-                        {isCurated && (
-                            <Checkbox style={{ padding: 0 }} checked={checkedCurate} onChange={handleChangeCurate} />
-                        )}
                     </Stack>
                 </CardContent>
             </BlankCard>
