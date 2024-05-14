048a840b07b22ea69f7b405d0c1aa5cd33430978
Matheus Paice <matheus.paice@jbtec.com.br>
2024-05-14 07:51:32 -0300

    fix: back rss

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -62,7 +62,7 @@ const Header = () => {
 
                 <Box flexGrow={1} />
                 <Stack spacing={1} direction="row" alignItems="center">
-                    {/** <Rss /> */}
+                    <Rss />
                     <Language />
                     {isLogged && <AvatarProfile />}
                 </Stack>
