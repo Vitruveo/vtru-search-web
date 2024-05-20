b2af095de63f87944f33db43c418fc35f47e878d
Guilherme <xxguilhermesoaresxx@gmail.com>
2024-05-20 11:52:53 -0300

    feat: add toastr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,22 +6,32 @@ import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { PersistGate } from 'redux-persist/integration/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import '@/utils/i18n';
-import 'toastr/build/toastr.min.css';
+import 'toastr/build/toastr.css';
+import 'toastr/build/toastr.min';
 import 'react-image-crop/dist/ReactCrop.css';
 
 import store, { persistor } from '@/store';
 import { NextAppDirEmotionCacheProvider } from '@/utils/theme/EmotionCache';
 import { ThemeSettings } from '@/utils/theme/Theme';
 import { themeConfig } from '@/utils/theme/ThemeConfig';
+import CustomizedSnackbar from './common/toastr';
+import { useToastr } from './hooks/useToastr';
 
 const inter = Inter({ subsets: ['latin'] });
 
 const MyApp = ({ children }: { children: React.ReactNode }) => {
     const theme = ThemeSettings();
+    const toastr = useToastr();
 
     return (
         <NextAppDirEmotionCacheProvider options={{ key: 'modernize' }}>
             <ThemeProvider theme={createTheme(themeConfig(theme))}>
+                <CustomizedSnackbar
+                    open={toastr.data.open}
+                    type={toastr.data.type}
+                    message={toastr.data.message}
+                    setOpenState={toastr.setState}
+                />
                 <CssBaseline />
                 {children}
             </ThemeProvider>
