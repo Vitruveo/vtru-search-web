d554ec3b7c40c7f49c1e78e1423a9c836617480c
Luan Almeida <luan.almeida@jbtec.com.br>
2024-05-24 11:47:28 -0300

    feat: make css to counter filter

--- src/app/components/Assets/assetsGrid/AssetFilterAccordion.tsx
+++ src/app/components/Assets/assetsGrid/AssetFilterAccordion.tsx
@@ -1,13 +1,14 @@
 import { useToggle } from '@/app/hooks/useToggle';
-import { Accordion, AccordionSummary, AccordionDetails, Box, Typography } from '@mui/material';
+import { Accordion, AccordionSummary, AccordionDetails, Box, Typography, Paper } from '@mui/material';
 import { IconMenu2 } from '@tabler/icons-react';
 
 interface AssetFilterAccordionProps {
     title: string;
     children: React.ReactNode;
+    numberOfFilters?: number;
 }
 
-export const AssetFilterAccordion = ({ title, children }: AssetFilterAccordionProps) => {
+export const AssetFilterAccordion = ({ title, children, numberOfFilters }: AssetFilterAccordionProps) => {
     const { toggle, isActive: expanded } = useToggle();
 
     return (
@@ -17,14 +18,33 @@ export const AssetFilterAccordion = ({ title, children }: AssetFilterAccordionPr
                     <Typography fontSize="1.2rem" fontWeight="700">
                         {title}
                     </Typography>
-
-                    <IconMenu2
-                        size="20"
-                        style={{
-                            transform: expanded ? 'rotate(90deg)' : 'rotate(0deg)',
-                            transition: 'transform 0.3s',
-                        }}
-                    />
+                    <Box display="flex" gap={10}>
+                        {numberOfFilters && (
+                            <Paper
+                                variant="elevation"
+                                sx={{
+                                    backgroundColor: '#00d6f4',
+                                    color: 'white',
+                                    borderRadius: 10,
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    padding: '0.2rem 0.6rem',
+                                }}
+                            >
+                                <Typography fontSize="0.8rem" fontWeight="700">
+                                    {numberOfFilters}
+                                </Typography>
+                            </Paper>
+                        )}
+                        <IconMenu2
+                            size="20"
+                            style={{
+                                transform: expanded ? 'rotate(90deg)' : 'rotate(0deg)',
+                                transition: 'transform 0.3s',
+                            }}
+                        />
+                    </Box>
                 </Box>
             </AccordionSummary>
             <AccordionDetails>
