e9e4e54a2d352b471872f7b5946bdcb3085dfb73
Luan Almeida <luan.almeida@jbtec.com.br>
2024-07-30 11:46:30 -0300

    feat: grid part in modal publish stack

--- src/app/components/Assets/components/DrawerStack/GridStack.tsx
+++ src/app/components/Assets/components/DrawerStack/GridStack.tsx
@@ -4,13 +4,66 @@ import { Asset } from '@/features/assets/types';
 import { Box, Button, Typography } from '@mui/material';
 import Image from 'next/image';
 import { useState } from 'react';
+import { ShareButton } from './ShareButton';
 
 interface GridStackProps {
     selectedAssets: Asset[];
 }
+
+const sizes = {
+    '2x2': 40,
+    '3x3': 26,
+    '4x4': 19,
+} as { [key: string]: number };
+
 export default function GridStack({ selectedAssets }: GridStackProps) {
-    const [selected, setSelected] = useState('1');
+    const [selected, setSelected] = useState('2x2');
+    const [confirmedGrid, setConfirmedGrid] = useState(false);
     const { language } = useI18n();
+
+    const handleConfirmGrid = () => {
+        setConfirmedGrid(true);
+    };
+
+    if (confirmedGrid) {
+        return (
+            <>
+                <Typography>Now share your image grid with the world</Typography>
+                <Box display={'flex'} flexDirection={'row'} gap={5} mt={4} mb={4} justifyContent={'center'}>
+                    <Box
+                        display={'grid'}
+                        gridTemplateColumns={`repeat(${selected[0]}, 1fr)`}
+                        gap={0.5}
+                        p={1}
+                        border={'2px solid #23afdb'}
+                    >
+                        {Array.from({ length: Number(selected[0]) ** 2 }).map((_, index) => (
+                            <div
+                                key={index}
+                                style={{
+                                    backgroundColor: '#EEEEEE',
+                                    height: sizes[selected] * 2,
+                                    width: sizes[selected] * 2,
+                                }}
+                            >
+                                {selectedAssets[index] && (
+                                    <Image
+                                        src={`${AWS_BASE_URL_S3}/${selectedAssets[index]?.formats?.preview?.path}`}
+                                        width={sizes[selected] * 2}
+                                        height={sizes[selected] * 2}
+                                        alt={'asset in grid 2x2'}
+                                    />
+                                )}
+                            </div>
+                        ))}
+                    </Box>
+                </Box>
+                <Box display={'flex'} justifyContent={'center'}>
+                    <ShareButton twitterURL={''} videoURL="" />
+                </Box>
+            </>
+        );
+    }
 
     return (
         <>
@@ -22,16 +75,23 @@ export default function GridStack({ selectedAssets }: GridStackProps) {
                     gridTemplateColumns={'repeat(2, 1fr)'}
                     gap={0.5}
                     p={1}
-                    border={`2px solid ${selected === '1' ? '#23afdb' : '#EEEEEE'}`}
-                    onClick={() => setSelected('1')}
+                    border={`2px solid ${selected === '2x2' ? '#23afdb' : '#EEEEEE'}`}
+                    onClick={() => setSelected('2x2')}
                 >
                     {Array.from({ length: 4 }).map((_, index) => (
-                        <div key={index} style={{ backgroundColor: '#EEEEEE', height: '40px', width: '40px' }}>
+                        <div
+                            key={index}
+                            style={{
+                                backgroundColor: '#EEEEEE',
+                                height: sizes['2x2'],
+                                width: sizes['2x2'],
+                            }}
+                        >
                             {selectedAssets[index] && (
                                 <Image
                                     src={`${AWS_BASE_URL_S3}/${selectedAssets[index]?.formats?.preview?.path}`}
-                                    width={40}
-                                    height={40}
+                                    width={sizes['2x2']}
+                                    height={sizes['2x2']}
                                     alt={'asset in grid 2x2'}
                                 />
                             )}
@@ -43,16 +103,23 @@ export default function GridStack({ selectedAssets }: GridStackProps) {
                     gridTemplateColumns={'repeat(3, 1fr)'}
                     gap={0.5}
                     p={1}
-                    border={`2px solid ${selected === '2' ? '#23afdb' : '#EEEEEE'}`}
-                    onClick={() => setSelected('2')}
+                    border={`2px solid ${selected === '3x3' ? '#23afdb' : '#EEEEEE'}`}
+                    onClick={() => setSelected('3x3')}
                 >
                     {Array.from({ length: 9 }).map((_, index) => (
-                        <div key={index} style={{ backgroundColor: '#EEEEEE', height: '26px', width: '26px' }}>
+                        <div
+                            key={index}
+                            style={{
+                                backgroundColor: '#EEEEEE',
+                                height: sizes['3x3'],
+                                width: sizes['3x3'],
+                            }}
+                        >
                             {selectedAssets[index] && (
                                 <Image
                                     src={`${AWS_BASE_URL_S3}/${selectedAssets[index]?.formats?.preview?.path}`}
-                                    width={26}
-                                    height={26}
+                                    width={sizes['3x3']}
+                                    height={sizes['3x3']}
                                     alt={'asset in grid 3x3'}
                                 />
                             )}
@@ -64,16 +131,23 @@ export default function GridStack({ selectedAssets }: GridStackProps) {
                     gridTemplateColumns={'repeat(4, 1fr)'}
                     gap={0.5}
                     p={1}
-                    border={`2px solid ${selected === '3' ? '#23afdb' : '#EEEEEE'}`}
-                    onClick={() => setSelected('3')}
+                    border={`2px solid ${selected === '4x4' ? '#23afdb' : '#EEEEEE'}`}
+                    onClick={() => setSelected('4x4')}
                 >
                     {Array.from({ length: 16 }).map((_, index) => (
-                        <div key={index} style={{ backgroundColor: '#EEEEEE', height: '19px', width: '19px' }}>
+                        <div
+                            key={index}
+                            style={{
+                                backgroundColor: '#EEEEEE',
+                                height: sizes['4x4'],
+                                width: sizes['4x4'],
+                            }}
+                        >
                             {selectedAssets[index] && (
                                 <Image
                                     src={`${AWS_BASE_URL_S3}/${selectedAssets[index]?.formats?.preview?.path}`}
-                                    width={19}
-                                    height={19}
+                                    width={sizes['4x4']}
+                                    height={sizes['4x4']}
                                     alt={'asset in grid 4x4'}
                                 />
                             )}
@@ -81,8 +155,9 @@ export default function GridStack({ selectedAssets }: GridStackProps) {
                     ))}
                 </Box>
             </Box>
+
             <Typography variant="caption">Note: Grid is limited to the first 16 curated items.</Typography>
-            <Button disabled variant="contained" fullWidth>
+            <Button variant="contained" fullWidth onClick={handleConfirmGrid}>
                 {language['search.drawer.stack.button.publish'] as string}
             </Button>
         </>
