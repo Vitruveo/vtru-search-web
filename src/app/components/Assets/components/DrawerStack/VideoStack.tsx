642c8cb0d5ae375ce37bd87723e80930e561d85b
Luan Almeida <luan.almeida@jbtec.com.br>
2024-07-30 16:15:21 -0300

    style: separate published in separate if to avoid many ternals in ui code

--- src/app/components/Assets/components/DrawerStack/VideoStack.tsx
+++ src/app/components/Assets/components/DrawerStack/VideoStack.tsx
@@ -60,78 +60,72 @@ export default function VideoStack({ selectedAssets, title, selectedAudio, audio
         text: `${language['search.checkoutMyNewVideo']} ${createBackLink(selectedAssets)}`,
     });
 
+    if (published) {
+        return (
+            <>
+                <Box display="flex" justifyContent="center">
+                    {loadingVideo ? <CircularProgress /> : <Typography>Now share your video with the world</Typography>}
+                </Box>
+
+                {hasVideo && (
+                    <Box mt={2}>
+                        <video width="100%" controls>
+                            <source src={video} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </Box>
+                )}
+
+                {!loadingVideo && (
+                    <Box display={'flex'} justifyContent={'center'} mt={2}>
+                        <ShareButton twitterURL={twitterShareURL} videoURL={video} />
+                    </Box>
+                )}
+            </>
+        );
+    }
+
     return (
         <>
             <Box display={'flex'} justifyContent={'center'}>
-                {!published ? (
-                    <Typography> 📹 Let is make a video from your amazing curation</Typography>
-                ) : (
-                    <Typography>Now share your video with the world</Typography>
-                )}
+                <Typography> 📹 Let is make a video from your amazing curation</Typography>
             </Box>
-            {!published && (
-                <Box display="flex" alignItems="center" mb={2} mt={2}>
-                    <Typography width={150}>{language['search.drawer.stack.sound'] as string}</Typography>
-                    <Select
-                        defaultValue={audios[0].value}
-                        onChange={(event) => {
-                            audio.pause();
-                            setIsPlaying(false);
-                            setSelectedAudio(event.target.value);
-                        }}
-                        fullWidth
-                    >
-                        {audios.map((item) => (
-                            <MenuItem key={item.value} value={item.value}>
-                                {item.name}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <Button
-                        sx={{ marginLeft: 1 }}
-                        size="small"
-                        variant="outlined"
-                        onClick={() => setIsPlaying((prev) => !prev)}
-                    >
-                        {isPlaying ? <IconPlayerPause /> : <IconPlayerPlay />}
-                    </Button>
-                </Box>
-            )}
-
-            {!published && <Box height={150} />}
-
-            {loadingVideo && (
-                <Box display="flex" justifyContent="center" mt={3}>
-                    <CircularProgress />
-                </Box>
-            )}
-
-            {hasVideo && (
-                <Box mt={2}>
-                    <video width="100%" controls>
-                        <source src={video} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
-                </Box>
-            )}
-
-            {!published ? (
-                <>
-                    <Typography variant="caption">Note: Video is limited to first 16 curated items.</Typography>
-                    <Button
-                        disabled={loadingVideo || selectedAssets.length === 0 || title.length === 0}
-                        variant="contained"
-                        fullWidth
-                        onClick={handleDispatchMakeVideo}
-                    >
-                        {language['search.drawer.stack.button.publish'] as string}
-                    </Button>{' '}
-                </>
-            ) : (
-                <Box display={'flex'} justifyContent={'center'} mt={2}>
-                    <ShareButton twitterURL={twitterShareURL} videoURL={video} />
-                </Box>
-            )}
+            <Box display="flex" alignItems="center" mb={2} mt={2}>
+                <Typography width={150}>{language['search.drawer.stack.sound'] as string}</Typography>
+                <Select
+                    defaultValue={audios[0].value}
+                    onChange={(event) => {
+                        audio.pause();
+                        setIsPlaying(false);
+                        setSelectedAudio(event.target.value);
+                    }}
+                    fullWidth
+                >
+                    {audios.map((item) => (
+                        <MenuItem key={item.value} value={item.value}>
+                            {item.name}
+                        </MenuItem>
+                    ))}
+                </Select>
+                <Button
+                    sx={{ marginLeft: 1 }}
+                    size="small"
+                    variant="outlined"
+                    onClick={() => setIsPlaying((prev) => !prev)}
+                >
+                    {isPlaying ? <IconPlayerPause /> : <IconPlayerPlay />}
+                </Button>
+            </Box>
+            <Box height={150} />
+            <Typography variant="caption">Note: Video is limited to first 16 curated items.</Typography>
+            <Button
+                disabled={loadingVideo || selectedAssets.length === 0 || title.length === 0}
+                variant="contained"
+                fullWidth
+                onClick={handleDispatchMakeVideo}
+            >
+                {language['search.drawer.stack.button.publish'] as string}
+            </Button>{' '}
         </>
     );
 }
