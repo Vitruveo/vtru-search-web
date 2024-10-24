accd32889f71cb2910532df26d17adf1af32c499
Luan Almeida <luan.almeida@jbtec.com.br>
2024-10-24 10:28:45 -0300

    hotfix: Drawer Asset height and width equal

--- src/app/components/Assets/components/DrawerAsset.tsx
+++ src/app/components/Assets/components/DrawerAsset.tsx
@@ -50,7 +50,7 @@ export function DrawerAsset({ drawerOpen, assetView, onClose }: Props) {
     return (
         <Drawer anchor="right" open={drawerOpen} onClose={onClose}>
             <Box p={4}>
-                <Box width={width} height={height} borderRadius={'8px'}>
+                <Box width={width} height={width} borderRadius={'8px'}>
                     <MediaRenderer
                         src={`${AWS_BASE_URL_S3}/${assetView?.formats?.preview?.path}`}
                         fallbackSrc={'https://via.placeholder.com/' + width + 'x' + height}
