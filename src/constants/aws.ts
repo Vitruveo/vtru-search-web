ad6cfaceb322534d57a861f59d20279d3bafca33
Matheus Paice <paice.zinner@gmail.com>
2025-01-17 12:50:03 -0300

    fix: temp stores envs

--- src/constants/aws.ts
+++ src/constants/aws.ts
@@ -1,3 +1,4 @@
 export const ASSET_STORAGE_URL = process.env.NEXT_PUBLIC_ASSET_STORAGE_URL || '';
 export const GENERAL_STORAGE_URL = process.env.NEXT_PUBLIC_GENERAL_STORAGE_URL || '';
-export const STORES_STORAGE_URL = process.env.NEXT_PUBLIC_STORES_STORAGE_URL || '';
+// export const STORES_STORAGE_URL = process.env.NEXT_PUBLIC_STORES_STORAGE_URL || '';
+export const STORES_STORAGE_URL = 'https://vitruveo-studio-dev-stores.s3.amazonaws.com';
