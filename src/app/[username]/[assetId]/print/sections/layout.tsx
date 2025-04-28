0c593e640a25b283aabdeafcff5b91f67ddd1491
Luan Almeida <luan.almeida@jbtec.com.br>
2025-04-28 18:58:55 -0300

    feat: add verification before go to print section

---
+++ src/app/[username]/[assetId]/print/sections/layout.tsx
@@ -0,0 +1,25 @@
+import { redirect } from 'next/navigation';
+import { API_BASE_URL } from '@/constants/api';
+
+interface SectionLayoutProps {
+    children: React.ReactNode;
+    params: {
+        username: string;
+        assetId: string;
+    };
+}
+
+export default async function SectionLayout({ children, params }: SectionLayoutProps) {
+    const { assetId, username } = params;
+
+    const assetRaw = await fetch(`${API_BASE_URL}/assets/store/${assetId}`);
+    const asset = (await assetRaw.json()).data;
+
+    const hasPrintAdded = asset.licenses.print.added;
+
+    if (!hasPrintAdded) {
+        redirect(`/${username}/${assetId}`);
+    }
+
+    return <div>{children}</div>;
+}
