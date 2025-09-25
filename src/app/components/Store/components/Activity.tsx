bed5017f64ba60721ac9bb2f367500eb49a88649
Luan Almeida <luan.almeida@jbtec.com.br>
2025-09-25 15:12:56 -0300

    feat: activity render download button

--- src/app/components/Store/components/Activity.tsx
+++ src/app/components/Store/components/Activity.tsx
@@ -1,6 +1,9 @@
+import { useAccount } from 'wagmi';
+import { useSelector } from '@/store/hooks';
 import { formatDate } from '@/utils/assets';
-import { Box, Card, Typography } from '@mui/material';
+import { Box, Button, Card, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { IconDownload } from '@tabler/icons-react';
 
 export interface ActivityProps {
     listing: {
@@ -12,13 +15,21 @@ export interface ActivityProps {
         };
         extra?: {
             url: string;
-            text: string;
         };
     }[];
+    downloadMedia: () => void;
+    mintAdress?: string;
 }
 
-export default function Activity({ listing }: ActivityProps) {
+export default function Activity({ listing, downloadMedia, mintAdress }: ActivityProps) {
     const theme = useTheme();
+    const isLogged = useSelector((state) => state.creator.token !== '');
+    const loggedWallets = useSelector((state) => state.creator.wallets);
+    const isMintAddressInLoggedWallets = loggedWallets.some((item) => item.address === mintAdress);
+
+    const { isConnected, address } = useAccount();
+    const isMintAddessEqualConnectedAddress = isConnected && address?.toLowerCase() === mintAdress?.toLowerCase();
+
     const formattedDate = (date: string | Date) => {
         const parsedDate = new Date(date);
         const day = parsedDate.getUTCDate();
@@ -26,6 +37,9 @@ export default function Activity({ listing }: ActivityProps) {
         const year = parsedDate.getUTCFullYear();
         return formatDate({ day, month, year });
     };
+
+    const downloadable = (title: string) =>
+        isLogged && title === 'Licensed' && (isMintAddressInLoggedWallets || isMintAddessEqualConnectedAddress);
 
     return (
         <div style={{ marginTop: '1px' }}>
@@ -42,19 +56,39 @@ export default function Activity({ listing }: ActivityProps) {
             >
                 Activity
             </Typography>
-            <Card style={{ borderRadius: 0 }}>
+            <Card style={{ borderRadius: 0, display: 'flex', flexDirection: 'column', gap: 10 }}>
                 {listing.length > 0 ? (
                     listing
                         .filter((item) => item.date)
                         .map((item, index) => (
-                            <Box key={index} display="grid" gridTemplateColumns="1fr 1fr 1fr">
-                                <Typography
-                                    variant="body1"
-                                    fontWeight="bold"
-                                    style={{ whiteSpace: 'nowrap', wordBreak: 'break-all' }}
-                                >
-                                    {item.title}
-                                </Typography>
+                            <Box
+                                key={index}
+                                display="grid"
+                                gridTemplateColumns={
+                                    downloadable(listing[0].title) ? '1fr 0.8fr 0.8fr 1fr' : '1fr 1fr 1fr'
+                                }
+                            >
+                                {item.extra?.url ? (
+                                    <a
+                                        href={item.extra.url}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        style={{
+                                            textDecoration: 'underline',
+                                            color: theme.palette.primary.main,
+                                        }}
+                                    >
+                                        {item.title}
+                                    </a>
+                                ) : (
+                                    <Typography
+                                        variant="body1"
+                                        fontWeight="bold"
+                                        style={{ whiteSpace: 'nowrap', wordBreak: 'break-all' }}
+                                    >
+                                        {item.title}
+                                    </Typography>
+                                )}
                                 <Typography
                                     variant="body1"
                                     style={{
@@ -63,34 +97,47 @@ export default function Activity({ listing }: ActivityProps) {
                                 >
                                     {formattedDate(item.date)}
                                 </Typography>
-                                <Box display="flex" gap={3}>
-                                    {item.link?.text && item.link?.url && (
-                                        <a
-                                            href={item.link.url}
-                                            target="_blank"
-                                            rel="noreferrer"
+                                {item.link?.text && item.link?.url && (
+                                    <a
+                                        href={item.link.url}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        style={{
+                                            textDecoration: 'underline',
+                                            color: theme.palette.primary.main,
+                                        }}
+                                    >
+                                        {item.link.text}
+                                    </a>
+                                )}
+                                {downloadable(item.title) && (
+                                    <Button
+                                        variant="contained"
+                                        onClick={downloadMedia}
+                                        sx={{
+                                            backgroundColor: theme.palette.primary.main,
+                                            color: '#ffff',
+                                            '&:hover': {
+                                                backgroundColor: theme.palette.primary.main,
+                                            },
+                                            borderRadius: 0,
+                                            height: 25,
+                                        }}
+                                    >
+                                        <IconDownload size={18} />
+                                        <Typography
+                                            fontSize={13}
                                             style={{
-                                                textDecoration: 'underline',
-                                                color: theme.palette.primary.main,
+                                                textTransform: 'none',
+                                                marginLeft: 5,
+                                                whiteSpace: 'nowrap',
+                                                textOverflow: 'clip',
                                             }}
                                         >
-                                            {item.link.text}
-                                        </a>
-                                    )}
-                                    {item.extra?.text && item.extra?.url && (
-                                        <a
-                                            href={item.extra.url}
-                                            target="_blank"
-                                            rel="noreferrer"
-                                            style={{
-                                                textDecoration: 'underline',
-                                                color: theme.palette.primary.main,
-                                            }}
-                                        >
-                                            {item.extra.text}
-                                        </a>
-                                    )}
-                                </Box>
+                                            Download Original
+                                        </Typography>
+                                    </Button>
+                                )}
                             </Box>
                         ))
                 ) : (
