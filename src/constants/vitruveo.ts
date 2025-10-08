b0d3734e3c24fa605d71c203f103cae4d6862b97
Luan Almeida <luan.almeida@jbtec.com.br>
2025-10-08 13:45:41 -0300

    hotfix: pit vitruveo urls as default

--- src/constants/vitruveo.ts
+++ src/constants/vitruveo.ts
@@ -1,5 +1,5 @@
 export const VITRUVEO_URL = process.env.NEXT_PUBLIC_VITRUVEO_URL || 'https://vitruveo.xyz';
 export const REDIRECTS_JSON =
-    process.env.NEXT_PUBLIC_REDIRECTS_JSON || 'https://luanbian.github.io/redirects-vtru/data.json';
+    process.env.NEXT_PUBLIC_REDIRECTS_JSON || 'https://vitruveo.github.io/vtru-settings/redirects.json';
 export const SYSTEM_STATUS_JSON =
-    process.env.NEXT_PUBLIC_SYSTEM_STATUS_JSON || 'https://luanbian.github.io/system-status-vtru/data.json';
+    process.env.NEXT_PUBLIC_SYSTEM_STATUS_JSON || 'https://vitruveo.github.io/vtru-settings/system-status.json';
